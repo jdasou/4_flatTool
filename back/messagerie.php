8b9c92e091172eba8fcce881a303592f838133f1
dasou <jeremy.souda@orange.fr>
2023-03-17 16:46:06 +0100

    flatool

--- back/messagerie.php
+++ back/messagerie.php
@@ -16,32 +16,18 @@ if(isset($_SESSION['id_compte']))
             {
             case "afficher_message":
             
-            //on selectionne tous les contacts triés par date décroissante
-            $requete="SELECT * FROM contacts ORDER BY date_contact DESC";
-            $resultat=mysqli_query($connexion,$requete);
-            //tant que $resultat contient des lignes (uplets)
-            $content="";
-            while($ligne=mysqli_fetch_object($resultat))
-                {
-                $content.="<details>"; 
 
-                $content.="<summary>"; 
-                $content.="<div>". $ligne->date_contact ."</div>"; 
-                $content.="<div>".$ligne->nom_contact . " " . $ligne->prenom_contact ."</div>"; 
-                $content.="<div>". $ligne->email_contact ."</div>"; 
-                $content.="<div><a href=\"#\">supprimer</a></div>"; 
-                $content.="</summary>"; 
-
-                $content.="<div>" . $ligne->message_contact . "</div>";
-
-                $content.="</details>"; 
-                }
 
             
             break;
 
             case "avertir_message":
 
+                if (isset($_GET['id_contact'])){
+                    $confirmation="<p>Voulez-vous supprimer le message n".$_GET['id_contact']."</p>";
+                    $confirmation="<a href=\"\">oui</a>&nbsp;&nbsp;&nbsp;";
+                    $confirmation="<a href=\"back.php?action=messagerie&cas=\">non</a> ";
+                }
             break;
 
             case "supprimer_message":
@@ -49,7 +35,26 @@ if(isset($_SESSION['id_compte']))
             break;
             }     
         }
- 
+        //on selectionne tous les contacts triés par date décroissante
+        $requete="SELECT * FROM contacts ORDER BY date_contact DESC";
+        $resultat=mysqli_query($connexion,$requete);
+        //tant que $resultat contient des lignes (uplets)
+        $content="";
+        while($ligne=mysqli_fetch_object($resultat))
+        {
+            $content.="<details>";
+
+            $content.="<summary>";
+            $content.="<div>". $ligne->date_contact ."</div>";
+            $content.="<div>".$ligne->nom_contact . " " . $ligne->prenom_contact ."</div>";
+            $content.="<div>". $ligne->email_contact ."</div>";
+            $content.="<div><a href=\"back.php?action=messagerie&cas=avertir_message&id_contact=" .$ligne->id_contact."\">supprimer</a></div>";
+            $content.="</summary>";
+
+            $content.="<div>" . $ligne->message_contact . "</div>";
+
+            $content.="</details>";
+        }
     //on referme la connexion
     mysqli_close($connexion);
     }
