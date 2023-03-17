348a047fef9a281ebf500bc04854ed915170bbaa
dasou <jeremy.souda@orange.fr>
2023-03-17 16:26:31 +0100

    flatool

--- back/back.php
+++ back/back.php
@@ -1,66 +1,49 @@
+ <?php
+//permet d'autoriser l'usage des variables de session
+session_start();
 
-<?php
-    //permet d'autoriser l'usage des variable de session
-
-    session_start();
-    //on teste si la variable de sessions $_SESSION['id_compte']
-
-    if (isset($_SESSION['id_compte'])){
-
-
-
-
-
-    //on connect le fichier de fonctions
+//on teste si la variable de session S_SESSION['id_compte'] 
+//existe
+if(isset($_SESSION['id_compte']))
+    {
+    //on connecte le fichier de fonctions
     require_once("../outils/fonctions.php");
 
-
-
-    //on etablie une connection avec la base de donnée
+    //on établit une connexion avec la BDD
     $connexion=connexion();
 
-
-
-    // formulaire de contact
-
     if(isset($_GET['action']))
-    {
+        {
 
-         //on switche sur la valeur contenue dans $_GET['action']
-         switch($_GET['action'])
+        //on switche sur la valeur contenue dans $_GET['action']
+        switch($_GET['action'])
+            {
+            case "logout":
+            //détruit toutes les variables de SESSION qui ont été enregistrées
+            session_destroy();
+            //on redirige vers la page d'accueil du site
+            header("Location:../index.php");
+            break;
 
-         {
-             case "logout":
-                 session_destroy();
-                 //detruit toutes les varibles de SESSION qui on eté sauvegarder
-                 header("Location:../index.php");
-                 //on rediriqe vers la page d'acceuil du site
-             break;
+            case "messagerie":
 
-
-           case "messagerie":
-
-               include("messagerie.php");
+            include("messagerie.php");
 
             break;
-         }
+            }     
+        }
 
+
+    //permet de relier front.php avec front.html
+    include("back.html");
+
+mysqli_close($connexion);
     }
+//on referme la connexion
 
-
-        //permet de relier font.php avec front.html
-        include("back.html");
-
-    //on ferme la connexion
-    mysqli_close($connexion);
-    }
-
-    else{
-    //l'utilisateur n'est pas autoriser
+else{
+    //l'utilisateur n'est pas autorisé
     header("Location:../log/login.php");
     }
-?>
 
 
-
-    
--- back/messagerie.php
+++ back/messagerie.php
@@ -1,80 +1,61 @@
-
-<?php
-//permet d'autoriser l'usage des variable de session
-
-
-//on teste si la variable de sessions $_SESSION['id_compte']
-
-if (isset($_SESSION['id_compte'])){
-
-    $titre="gestion de la messagerie";
-
-
-
-
-
-    //on connect le fichier de fonctions
+ <?php
+//on teste si la variable de session S_SESSION['id_compte'] existe
+if(isset($_SESSION['id_compte']))
+    {
+    $titre="Gestion de la messagerie";
+    //on connecte le fichier de fonctions
     require_once("../outils/fonctions.php");
 
-
-
-    //on etablie une connection avec la base de donnée
+    //on établit une connexion avec la BDD
     $connexion=connexion();
 
-
-
-    // formulaire de contact
-
     if(isset($_GET['cas']))
-    {
+        {
+        //on switche sur la valeur contenue dans $_GET['action']
+        switch($_GET['cas'])
+            {
+            case "afficher_message":
+            
+            //on selectionne tous les contacts triés par date décroissante
+            $requete="SELECT * FROM contacts ORDER BY date_contact DESC";
+            $resultat=mysqli_query($connexion,$requete);
+            //tant que $resultat contient des lignes (uplets)
+            $content="";
+            while($ligne=mysqli_fetch_object($resultat))
+                {
+                $content.="<details>"; 
 
-         //on switche sur la valeur contenue dans $_GET['action']
-         switch($_GET['cas'])
+                $content.="<summary>"; 
+                $content.="<div>". $ligne->date_contact ."</div>"; 
+                $content.="<div>".$ligne->nom_contact . " " . $ligne->prenom_contact ."</div>"; 
+                $content.="<div>". $ligne->email_contact ."</div>"; 
+                $content.="<div><a href=\"#\">supprimer</a></div>"; 
+                $content.="</summary>"; 
 
-         {
-             case "afficher_message":
-                 //on selectionne tous les contact trier pars date décroisssant
+                $content.="<div>" . $ligne->message_contact . "</div>";
 
-                 $requette="SELECT * FROM contacts ORDER BY date_contact DESC";
-                 $resultat=mysqli_query($connexion,$requette);
+                $content.="</details>"; 
+                }
 
-                 //tant que $resultat contien des ligne (uplets =ligne d'enregistrement)
-                $content="";
-                 while ($ligne=mysqli_fetch_object($resultat)){
+            
+            break;
 
+            case "avertir_message":
 
+            break;
 
-                 }
+            case "supprimer_message":
 
-             break;
-
-
-             case "avertir_message":
-
-             break;
-
-             case "supprimer_message":
-
-             break;
-
-               case "";
-         }
-
-    }
-
-
-        //permet de relier font.php avec front.html
-        include("back.html");
-
-
-    //on ferme la connexion
+            break;
+            }     
+        }
+ 
+    //on referme la connexion
     mysqli_close($connexion);
     }
 else{
-    //l'utilisateur n'est pas autoriser
+    //l'utilisateur n'est pas autorisé
     header("Location:../log/login.php");
-}
-    ?>
+    }
 
-
-    
+?>
